eafef50849ee34ffc264bcfe5320eb8a85b908a4
Wreckitelf <49493016+Wreckitelf@users.noreply.github.com>
2021-12-08 16:28:46 -0800

    Add files via upload

---
+++ ExtraMethods.java
@@ -0,0 +1,4 @@
+public interface ExtraMethods
+{
+    public void addOperator();
+}
---
+++ MathDriver.java
@@ -0,0 +1,8 @@
+public class MathDriver {
+    public static void main(String[] args)
+    {
+            Addition addition = new Addition(0, 1);
+            System.out.println(addition.toString());
+    }
+
+}
